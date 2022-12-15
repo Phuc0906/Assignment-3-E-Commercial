bf2959c94e4fee0d1357368fcce7b4c3a4376323
ThaiThuan <s3877024@rmit.edu.vn>
2022-12-15 14:04:16 +0700

    APIuser update

--- Server/index.js
+++ Server/index.js
@@ -1,25 +1,23 @@
-var mysql = require('mysql');
-const express = require('express');
+var mysql = require("mysql");
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const productAPI = require('./routes/ProductAPIs');
-
+const cors = require("cors");
+const productAPI = require("./routes/ProductAPIs");
+const userAPI = require("./routes/UserAPIs");
 app.use(cors());
-app.use('/product', productAPI);
-
+app.use("/product", productAPI);
+app.use("/user", userAPI);
 var connection = mysql.createConnection({
     host: "3.0.89.85",
     user: "phuchoang",
-    password: "Assignment3Password!"
+    password: "Assignment3Password!",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("Connect");
 });
 
-
-app.listen(3000, () => {
+app.listen(3004, () => {
     console.log("Server is listening");
-})
-
+});
---
+++ Server/routes/UserAPIs.js
@@ -0,0 +1,93 @@
+const cors = require("cors");
+const { query } = require("express");
+const express = require("express");
+const router = express.Router();
+
+router.use(cors());
+router.use(express.json());
+var mysql = require("mysql");
+
+var db = mysql.createConnection({
+    host: "3.0.89.85",
+    user: "g9",
+    password: "Ass3Password!",
+    database: "ASS3DB",
+});
+
+router.get("/login", async (req, res) => {
+    const email = req.query.email;
+    const password = req.query.password;
+
+    const queryCommand = `SELECT * FROM USER WHERE EMAIL = '${email}' && PASSWORD = '${password}'`;
+    db.query(queryCommand, (err, result) => {
+        if (err) {
+            res.send(err);
+        } else {
+            if (result.length > 0) {
+                res.send({
+                    verify: 1,
+                    role: result[0].ROLE,
+                });
+            } else {
+                res.send({
+                    verify: 0,
+                });
+            }
+        }
+    });
+});
+
+router.get("/infomation", (req, res) => {
+    const id = req.query.id;
+    const queryCommand = `SELECT * FROM USER WHERE ID = '${id}'`;
+    db.query(queryCommand, (err, result) => {
+        if (err) {
+            res.send(err);
+        } else {
+            if (result != null) {
+                res.send(result[0]);
+            }
+        }
+    });
+});
+
+router.post("/infomation", (req, res) => {
+    const reqBody = req.body;
+    const queryCommand = `
+    INSERT INTO USER(ID, NAME, ADDRESS, PHONE_NUMBER, GENDER, DATE_OF_BIRTH, POINT, PASSWORD, ROLE, EMAIL)
+    VALUES(NULL, '${reqBody.name}', '${reqBody.address}', '${reqBody.phone}', '${reqBody.gender}', '${reqBody.date}', 0, '${reqBody.password}', 'USER', '${reqBody.email}')`;
+    db.query(queryCommand, (err, result) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+router.patch("/password", (req, res) => {
+    const oldPass = req.query.oldPass;
+    const newPass = req.query.newPass;
+    const id = req.query.id;
+
+    const queryCommand = `UPDATE USER 
+                            SET PASSWORD = '${newPass}' 
+                            WHERE ID = ${id} &&  PASSWORD = '${oldPass}'`;
+    db.query(queryCommand, (err, result) => {
+        if (err) {
+            res.send(err);
+        } else {
+            if (result.changedRows > 0) {
+                res.send({
+                    verify: 1,
+                });
+            } else {
+                res.send({
+                    verify: 0,
+                });
+            }
+        }
+    });
+});
+
+module.exports = router;
